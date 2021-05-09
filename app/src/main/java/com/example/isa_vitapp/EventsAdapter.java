4d253c859162834c5acefe3c2cd4fc6d88a21f3d
Pranav Prakasan <pranav.prakasan2019@gmail.com>
2021-05-09 17:29:39 +0530

    Events list added, recycler view, db retrieval

--- app/src/main/java/com/example/isa_vitapp/EventsAdapter.java
+++ app/src/main/java/com/example/isa_vitapp/EventsAdapter.java
@@ -1,25 +1,36 @@
 package com.example.isa_vitapp;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.isa_vitapp.activity.EventsActivity;
 import com.squareup.picasso.Picasso;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Map;
 
+import soup.neumorphism.NeumorphButton;
 import soup.neumorphism.NeumorphCardView;
+import soup.neumorphism.ShapeType;
 
 public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventsViewHolder> {
     public Map<String, Object> mDataset;
+    public ArrayList<String> link_list, title_list, content_list, reg_list;
+    public ArrayList<Boolean> open_list;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
@@ -28,17 +39,28 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventsView
         // each data item is just a string in this case
         public ImageView poster;
         public NeumorphCardView detail_card;
+        public TextView title, content;
+        public NeumorphButton register_button;
 
         public EventsViewHolder(View v) {
             super(v);
             poster = v.findViewById(R.id.event_poster);
             detail_card = v.findViewById(R.id.details_card);
+            title = v.findViewById(R.id.event_name);
+            content = v.findViewById(R.id.detail);
+            register_button = v.findViewById(R.id.neumorphButton);
         }
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public EventsAdapter(Map<String, Object> myDataset) {
-        mDataset.putAll(myDataset);
+    public EventsAdapter(ArrayList<String> myDataset, ArrayList<String> titles, ArrayList<String> contents, ArrayList<Boolean> open, ArrayList<String> reg) {
+//        mDataset.putAll(myDataset);
+        link_list = myDataset;
+        title_list = titles;
+        content_list = contents;
+        open_list = open;
+        reg_list = reg;
+
     }
 
     // Create new views (invoked by the layout manager)
@@ -60,12 +82,15 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventsView
 
         try {
             Picasso.get()
-                    .load((Uri) mDataset.get("poster"))
+                    .load(link_list.get(position))
                     .into(holder.poster);
         } catch (Exception e) {
             Log.d("TAG", "No photo");
             e.printStackTrace();
         }
+
+        holder.title.setText(title_list.get(position));
+        holder.content.setText(content_list.get(position));
 
         holder.poster.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,6 +98,34 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventsView
                 Log.d("TAG", "No touch");
                 holder.poster.setVisibility(View.INVISIBLE);
                 holder.detail_card.setVisibility(View.VISIBLE);
+
+                if(open_list.get(position)){
+                    holder.register_button.setTextColor(Color.parseColor("#0C97E8"));
+
+                    holder.register_button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            String url = reg_list.get(position);
+                            Intent i = new Intent(Intent.ACTION_VIEW);
+                            i.setData(Uri.parse(url));
+                            v.getContext().startActivity(i);
+                        }
+                    });
+
+
+                }
+                else{
+                    holder.register_button.setShapeType(ShapeType.PRESSED);
+                }
+            }
+        });
+
+        holder.detail_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TAG", "No touch");
+                holder.poster.setVisibility(View.VISIBLE);
+                holder.detail_card.setVisibility(View.INVISIBLE);
 
             }
         });
@@ -82,7 +135,7 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventsView
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset.size();
+        return link_list.size();
     }
 
 }
--- app/src/main/java/com/example/isa_vitapp/activity/EventsActivity.java
+++ app/src/main/java/com/example/isa_vitapp/activity/EventsActivity.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,15 +20,97 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 
 public class EventsActivity extends AppCompatActivity {
 
     ArrayList<String> poster_links = new ArrayList<>();
+    ArrayList<String> title_list = new ArrayList<>();
+    ArrayList<String> content_list = new ArrayList<>();
+    ArrayList<Boolean> open_list = new ArrayList<>();
+    ArrayList<String> reg_list = new ArrayList<>();
+
     private DatabaseReference mDatabase;
-    Map<String, Object> details;
+
+    Map<String, Object> details = new Map<String, Object>() {
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean containsKey(@Nullable @org.jetbrains.annotations.Nullable Object key) {
+            return false;
+        }
+
+        @Override
+        public boolean containsValue(@Nullable @org.jetbrains.annotations.Nullable Object value) {
+            return false;
+        }
+
+        @Nullable
+        @org.jetbrains.annotations.Nullable
+        @Override
+        public Object get(@Nullable @org.jetbrains.annotations.Nullable Object key) {
+            return null;
+        }
+
+        @Nullable
+        @org.jetbrains.annotations.Nullable
+        @Override
+        public Object put(String key, Object value) {
+            return null;
+        }
+
+        @Nullable
+        @org.jetbrains.annotations.Nullable
+        @Override
+        public Object remove(@Nullable @org.jetbrains.annotations.Nullable Object key) {
+            return null;
+        }
+
+        @Override
+        public void putAll(@NonNull @NotNull Map<? extends String, ?> m) {
+
+        }
+
+        @Override
+        public void clear() {
+
+        }
+
+        @NonNull
+        @NotNull
+        @Override
+        public Set<String> keySet() {
+            return null;
+        }
+
+        @NonNull
+        @NotNull
+        @Override
+        public Collection<Object> values() {
+            return null;
+        }
+
+        @NonNull
+        @NotNull
+        @Override
+        public Set<Entry<String, Object>> entrySet() {
+            return null;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,22 +132,28 @@ public class EventsActivity extends AppCompatActivity {
                                 Log.d("TAG", document.getId() + " => " + document.getData());
                                 Toast.makeText(EventsActivity.this, document.getId(), Toast.LENGTH_SHORT).show();
 
-                                details = document.getData();
+                                details = (document.getData());
 
                                 Toast.makeText(EventsActivity.this, details.keySet().toString(), Toast.LENGTH_SHORT).show();
                                 Toast.makeText(EventsActivity.this, details.values().toString(), Toast.LENGTH_SHORT).show();
 
+                                poster_links.add((String) details.get("poster"));
+                                title_list.add((String) details.get("title"));
+                                content_list.add((String) details.get("content"));
+                                open_list.add((Boolean) details.get("open"));
+                                reg_list.add((String) details.get("link"));
+
+                                Log.d("TAG", "Links" + poster_links.toString());
+
                                 try {
-                                    if(details.size() > 2) {
-                                        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-                                        recyclerView.setAdapter(new EventsAdapter(details));
-                                    }
+                                    Log.d("TAG", "Reached here" + details.toString());
+                                    recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                                    recyclerView.setAdapter(new EventsAdapter(poster_links, title_list, content_list, open_list, reg_list));
+
                                 } catch (Exception e) {
                                     e.printStackTrace();
                                 }
                             }
-
-
 
                         } else {
                             Log.d("TAG", "Error getting documents: ", task.getException());
